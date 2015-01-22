23443e0f24680430f1fda328daba13467b2b4d08
buckie <wjmartino@gmail.com>
2015-01-22 10:26:27 -0500

    finished the chapter, now onto the exercises
    Signed-off-by: buckie <wjmartino@gmail.com>

--- src/Scheme48/Eval.hs
+++ src/Scheme48/Eval.hs
@@ -7,7 +7,32 @@ import Scheme48.Parsers (parseExprs)
 
 eval :: LispVal -> LispVal
 eval (List [Atom "quote", val]) = val
+eval (List (Atom func:args)) = apply func $ map eval args
 eval val = val
+
+apply :: String -> [LispVal] -> LispVal
+apply fn args = maybe (Bool False) ($ args) $ lookup fn primitives
+
+primitives :: [(String, [LispVal] -> LispVal)]
+primitives = [("+", numericBinop (+)),
+              ("-", numericBinop (-)),
+              ("*", numericBinop (*)),
+              ("/", numericBinop div),
+              ("mod", numericBinop mod),
+              ("quotient", numericBinop quot),
+              ("remainder", numericBinop rem)]
+
+numericBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> LispVal
+numericBinop op params = Number $ foldl1 op $ map unpackNum params
+
+unpackNum :: LispVal -> Integer
+unpackNum (Number n) = n
+unpackNum (String n) = let parsed = reads n :: [(Integer, String)] in
+                            if null parsed
+                              then 0
+                              else fst $ parsed !! 0
+unpackNum (List [n]) = unpackNum n
+unpackNum _ = 0
 
 readExpr :: String -> LispVal
 readExpr input = case parseExprs input of
